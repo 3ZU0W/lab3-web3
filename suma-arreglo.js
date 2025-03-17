04d2101ef5e37828d07a1e66e409e63cbf50c947
Matt Logan Tahuasi Pariamo <mtahuasip@fcpn.edu.bo>
2025-03-17 12:08:22 -0400

    Actualizado suma-arreglo.js

--- suma-arreglo.js
+++ suma-arreglo.js
@@ -3,3 +3,6 @@ const sumarNumeros = (numeros) =>
 
 const numeros = [1, 2, 3, 4, 5];
 console.log(`La suma es: ${sumarNumeros(numeros)}`);
+
+const numeros2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(`La suma es: ${sumarNumeros(numeros2)}`);
